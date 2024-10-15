2b4fda679131a4e2e7ee70c75739e5bd258791a2
Juanma Canovas <juanmacanovas13@gmail.com>
2024-10-14 23:17:18 -0300

    cards mejoradas y carrusel corregido

--- Entrega2/js/carruseles.js
+++ Entrega2/js/carruseles.js
@@ -1,32 +1,4 @@
-/*const buttonPrev = document.getElementById('button-prev-multiP');
-const buttonNext = document.getElementById('button-next-multiP');
-const track = document.getElementById('track');
-const carruselMP =  document.getElementById('carousel-multiplayer');
-const multiPlayerCards = document.querySelectorAll('.slick');
 
-track.style.left = '0px';
-
-const slickWidth = multiPlayerCards[0].offsetWidth;
-
-buttonPrev.onclick = () => Move(1);
-buttonNext.onclick = () => Move(2);
-
-function Move(value){
-    const trackWidth = track.scrollWidth; // Cambiado a scrollWidth para obtener el tamaño total del contenido
-    const carruselMpWidth = carruselMP.offsetWidth;
-
-    // Si left no está definido, la posición es 0
-    let leftPosition = track.style.left === "" ? 0 : parseFloat(track.style.left.replace('px', ''));
-
-    // Si se mueve hacia adelante
-    if (leftPosition > -(trackWidth - carruselMpWidth) && value === 2) {
-        track.style.left = `${leftPosition - slickWidth}px`; // Desplazamiento a la izquierda
-    }
-    // Si se mueve hacia atrás
-    else if (leftPosition < 0 && value === 1) {
-        track.style.left = `${leftPosition + slickWidth}px`; // Desplazamiento a la derecha
-    }
-}*/
 
 document.querySelectorAll('.carousel').forEach(carousel => {
     const buttonPrev = carousel.querySelector('[data-button="button-prev"]');
@@ -36,8 +8,8 @@ document.querySelectorAll('.carousel').forEach(carousel => {
     const slickItems = carousel.querySelectorAll('.slick');
 
     // Establecer la posición inicial del track
-    track.style.left = '0px';
-
+    track.style.left = '30px';
+    buttonPrev.classList.add('invisible');
     // Obtener el ancho de un item
     const slickWidth = slickItems[0].offsetWidth;
 
@@ -45,10 +17,14 @@ document.querySelectorAll('.carousel').forEach(carousel => {
     buttonNext.onclick = () => moveTrack(2);
 
     function moveTrack(direction) {
+        if(track.style.left=='30px'){
+            buttonPrev.classList.add('invisible');
+        }
+        buttonPrev.classList.remove('invisible');
         const trackWidth = track.scrollWidth;
         const carruselWidth = carruselElement.offsetWidth;
         let leftPosition = track.style.left === "" ? 0 : parseFloat(track.style.left.replace('px', ''));
-
+        
         // Mover hacia adelante
         if (leftPosition > -(trackWidth - carruselWidth) && direction === 2) {
             track.style.left = `${leftPosition - slickWidth}px`;
@@ -56,6 +32,9 @@ document.querySelectorAll('.carousel').forEach(carousel => {
         // Mover hacia atrás
         else if (leftPosition < 0 && direction === 1) {
             track.style.left = `${leftPosition + slickWidth}px`;
+        }
+        if(track.style.left=='30px'){
+            buttonPrev.classList.add('invisible');
         }
     }
 });
